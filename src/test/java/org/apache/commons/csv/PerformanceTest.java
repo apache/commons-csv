d8eaf1cd616a1e92c76ed6356790a907135c3d81
Sebastian Bazley <sebb@apache.org>
2012-10-16 15:25:51 +0000

    Alignment
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/csv/trunk@1398847 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/csv/PerformanceTest.java
+++ src/test/java/org/apache/commons/csv/PerformanceTest.java
@@ -226,8 +226,7 @@ public class PerformanceTest {
 
    private static Constructor<Lexer> getLexerCtor(final String clazz) throws Exception {
        @SuppressWarnings("unchecked")
-    final
-       Class<Lexer> lexer = (Class<Lexer>) Class.forName("org.apache.commons.csv."+clazz);
+       final Class<Lexer> lexer = (Class<Lexer>) Class.forName("org.apache.commons.csv."+clazz);
        final Constructor<Lexer> ctor = lexer.getConstructor(new Class<?>[]{CSVFormat.class, ExtendedBufferedReader.class});
        return ctor;
    }
