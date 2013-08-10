04a2e9ebe4933d41c84b95074d57aa1da52cc520
Benedikt Ritter <britter@apache.org>
2013-08-10 10:46:03 +0000

    Remove unused method. It made no sense anyway, since Assert.assertArrayEquals is capable of comparing multi dimensional arrays.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/csv/trunk@1512617 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/csv/Utils.java
+++ src/test/java/org/apache/commons/csv/Utils.java
@@ -33,20 +33,6 @@ final class Utils {
     }
 
     /**
-     * Checks if the two 2d arrays have identical contents.
-     *
-     * @param message the message to be displayed
-     * @param expected the 2d array of expected results
-     * @param actual the 2d array of actual results
-     */
-    public static void compare(final String message, final String[][] expected, final String[][] actual) {
-        Assert.assertEquals(message+"  - outer array size", expected.length, actual.length);
-        for(int i = 0; i < expected.length; i++) {
-            Assert.assertArrayEquals(message+" (entry "+i+")",expected[i], actual[i]);
-        }
-    }
-
-    /**
      * Checks if the 2d array has the same contents as the list of records.
      *
      * @param message the message to be displayed
