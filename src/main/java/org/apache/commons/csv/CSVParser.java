966cd208ca18c299c6c765d4e1440f0877d96981
Gary D. Gregory <ggregory@apache.org>
2014-04-07 14:50:26 +0000

    [CSV-110] Add ability to parse single lines.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/csv/trunk@1585496 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/csv/CSVParser.java
+++ src/main/java/org/apache/commons/csv/CSVParser.java
@@ -435,7 +435,7 @@ public final class CSVParser implements Iterable<CSVRecord>, Closeable {
      * @throws IOException
      *             on parse error or input read-failure
      */
-    CSVRecord nextRecord() throws IOException {
+    public CSVRecord nextRecord() throws IOException {
         CSVRecord result = null;
         this.record.clear();
         StringBuilder sb = null;
