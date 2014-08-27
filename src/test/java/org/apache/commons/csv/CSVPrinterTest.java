2a318dcf33150f0a799443710b48dcc45ef72b83
Gary D. Gregory <ggregory@apache.org>
2014-08-27 13:46:31 +0000

    Oops, back out one of the previous changes.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/csv/trunk@1620896 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/csv/CSVPrinterTest.java
+++ src/test/java/org/apache/commons/csv/CSVPrinterTest.java
@@ -169,7 +169,7 @@ public class CSVPrinterTest {
     public void testExcelPrintAllArrayOfLists() throws IOException {
         final StringWriter sw = new StringWriter();
         final CSVPrinter printer = new CSVPrinter(sw, CSVFormat.EXCEL);
-        printer.printRecords((Object) new List[] { Arrays.asList("r1c1", "r1c2"), Arrays.asList("r2c1", "r2c2") });
+        printer.printRecords(new List[] { Arrays.asList("r1c1", "r1c2"), Arrays.asList("r2c1", "r2c2") });
         assertEquals("r1c1,r1c2" + recordSeparator + "r2c1,r2c2" + recordSeparator, sw.toString());
         printer.close();
     }
