016cc839d2e059e25217c7929a1dbbf41b0f2135
Gary D. Gregory <ggregory@apache.org>
2014-10-29 14:31:37 +0000

    Checkstyle: line length.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/csv/trunk@1635147 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/csv/CSVRecord.java
+++ src/main/java/org/apache/commons/csv/CSVRecord.java
@@ -50,7 +50,8 @@ public final class CSVRecord implements Serializable, Iterable<String> {
     /** The values of the record */
     private final String[] values;
 
-    CSVRecord(final String[] values, final Map<String, Integer> mapping, final String comment, final long recordNumber, long characterPosition) {
+    CSVRecord(final String[] values, final Map<String, Integer> mapping, final String comment, final long recordNumber,
+            long characterPosition) {
         this.recordNumber = recordNumber;
         this.values = values != null ? values : EMPTY_STRING_ARRAY;
         this.mapping = mapping;
