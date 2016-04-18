2210c0b02f9b7c7b9e2eac420aef7ceab3e4824b
Gary D. Gregory <ggregory@apache.org>
2016-04-18 01:15:38 +0000

    Sort members.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/csv/trunk@1739676 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/csv/Constants.java
+++ src/main/java/org/apache/commons/csv/Constants.java
@@ -24,38 +24,32 @@ package org.apache.commons.csv;
  */
 final class Constants {
 
+    static final char BACKSLASH = '\\';
+    
     static final char BACKSPACE = '\b';
+
     static final char COMMA = ',';
 
     /**
      * Starts a comment, the remainder of the line is the comment.
      */
     static final char COMMENT = '#';
-
+    
     static final char CR = '\r';
-    static final Character DOUBLE_QUOTE_CHAR = Character.valueOf('"');
-    static final char BACKSLASH = '\\';
-    static final char FF = '\f';
-    static final char LF = '\n';
-    static final char SP = ' ';
-    static final char TAB = '\t';
-
-    /** ASCII record separator */
-    static final char RS = 30;
-
-    /** ASCII unit separator */
-    static final char US = 31;
-
-    static final String EMPTY = "";
-
-    /** The end of stream symbol */
-    static final int END_OF_STREAM = -1;
-
-    /** Undefined state for the lookahead char */
-    static final int UNDEFINED = -2;
-
+    
     /** RFC 4180 defines line breaks as CRLF */
     static final String CRLF = "\r\n";
+    
+    static final Character DOUBLE_QUOTE_CHAR = Character.valueOf('"');
+    
+    static final String EMPTY = "";
+    
+    /** The end of stream symbol */
+    static final int END_OF_STREAM = -1;
+    
+    static final char FF = '\f';
+
+    static final char LF = '\n';
 
     /**
      * Unicode line separator.
@@ -63,13 +57,26 @@ final class Constants {
     static final String LINE_SEPARATOR = "\u2028";
 
     /**
+     * Unicode next line.
+     */
+    static final String NEXT_LINE = "\u0085";
+
+    /**
      * Unicode paragraph separator.
      */
     static final String PARAGRAPH_SEPARATOR = "\u2029";
 
-    /**
-     * Unicode next line.
-     */
-    static final String NEXT_LINE = "\u0085";
+    /** ASCII record separator */
+    static final char RS = 30;
+
+    static final char SP = ' ';
+
+    static final char TAB = '\t';
+
+    /** Undefined state for the lookahead char */
+    static final int UNDEFINED = -2;
+
+    /** ASCII unit separator */
+    static final char US = 31;
 
 }
