6780f0d96cf89d3f781809233f0b01ee91d1b264
Gary Gregory <ggregory@apache.org>
2016-07-30 11:23:32 -0700

    Remove unused import.

--- src/main/java/org/apache/commons/csv/CSVFormat.java
+++ src/main/java/org/apache/commons/csv/CSVFormat.java
@@ -36,7 +36,6 @@ import java.io.Reader;
 import java.io.Serializable;
 import java.io.StringWriter;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
