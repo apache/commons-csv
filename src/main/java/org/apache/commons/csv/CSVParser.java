b5de127552c70c88dfca26714279d745845d93ee
Gary Gregory <ggregory@apache.org>
2016-08-23 14:42:06 -0700

    Remove unused import.

--- src/main/java/org/apache/commons/csv/CSVParser.java
+++ src/main/java/org/apache/commons/csv/CSVParser.java
@@ -27,7 +27,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringReader;
-import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
