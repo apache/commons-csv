476e8830818f6f31481fa487cf62ffb407b2f4e2
Gary Gregory <gardgregory@gmail.com>
2019-05-19 19:50:41 -0400

    Remove unused import.

--- src/main/java/org/apache/commons/csv/CSVParser.java
+++ src/main/java/org/apache/commons/csv/CSVParser.java
@@ -34,7 +34,6 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
