47424530a5f46456e9d6a986a29beb249cded607
Gary Gregory <garydgregory@gmail.com>
2024-09-19 15:12:10 -0400

    Sort imports

--- src/test/java/org/apache/commons/csv/CSVParserTest.java
+++ src/test/java/org/apache/commons/csv/CSVParserTest.java
@@ -23,11 +23,11 @@ import static org.apache.commons.csv.Constants.LF;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 import java.io.File;
 import java.io.IOException;
