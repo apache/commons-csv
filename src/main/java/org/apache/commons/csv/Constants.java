65382cad1fa0f0d8336e6686c2690fc617c527c7
Gary Gregory <garydgregory@gmail.com>
2025-03-13 15:05:38 -0400

    Remove latin acronym in comment

--- src/main/java/org/apache/commons/csv/Constants.java
+++ src/main/java/org/apache/commons/csv/Constants.java
@@ -40,7 +40,7 @@ final class Constants {
     /** RFC 4180 defines line breaks as CRLF */
     static final String CRLF = "\r\n";
 
-    static final Character DOUBLE_QUOTE_CHAR = Character.valueOf('"');  // N.B. Explicit (un)boxing is intentional
+    static final Character DOUBLE_QUOTE_CHAR = Character.valueOf('"');  // Explicit (un)boxing is intentional
 
     static final String EMPTY = "";
 
